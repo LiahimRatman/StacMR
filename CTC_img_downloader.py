8f25a28249f10b58324b6156b5e4afa4a4b320df
Mikhail Korotkov <ma-korotkov@yandex.ru>
2021-10-18 01:28:41 +0300

    StacMR | CTC_img_downloader.py | fix code a little bit

--- CTC_img_downloader.py
+++ CTC_img_downloader.py
@@ -3,11 +3,21 @@ import json
 import os
 from tqdm import tqdm
 
-with open ('CTC_anns.json', 'r') as fp:
-    ctc_anns = json.load(fp)
 
-os.mkdir('./images/')
+def download_ctc_images(ctc_annotations_name='CTC_anns.json', dir_name='./images/'):
+    """
+    Downloading CTC_images from web using given annotation.json file
+    :param ctc_annotations_name: information about CTC Dataset
+    :param dir_name: New dir for images
+    :return:
+    """
+    print("Reading annotations...")
+    with open(ctc_annotations_name, 'r') as fp:
+        ctc_anns = json.load(fp)
 
-for item in tqdm(ctc_anns['images']):
-    urllib.request.urlretrieve(item['coco_url'], './images/'+ item['coco_url'].strip().split('/')[-1])
+    print("Making new dir for CTC images...")
+    os.mkdir(dir_name)
 
+    print("Downloading CTC images from web to new dir")
+    for item in tqdm(ctc_anns['images']):
+        urllib.request.urlretrieve(item['coco_url'], dir_name + item['coco_url'].strip().split('/')[-1])
